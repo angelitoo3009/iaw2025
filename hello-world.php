47e16f1dda287b6401010682849fd8b393aca552
Angel Delgado <angel.delgado.betka.al@iespoligonosur.org>
2025-02-24 13:59:35 +0100

    dsd

--- hello-world.php
+++ hello-world.php
@@ -7,8 +7,23 @@
 </head>
 <body>
     <?php
-        define ('ALTURA', "Hola Mundo, desde el IES polígonosur");
-        echo ALTURA;
+        $name = "Angel";
+        echo "Me gusta php y lo estudio gracias a ". $name . "!" . "<b>";
+        echo "<br>";   
+        var_dump($name);
+        echo "<br>";
+
+        $salario = 1920;
+        echo "Mi salario base es de ". $salario ."<b>";
+        echo "<br>";   
+        var_dump($salario);
+        echo "<br>";
+
+        $trabajo = "Informatica";
+        echo "Trabajo en el sector de la ". $trabajo . "<b>";
+        echo "<br>";   
+        var_dump($trabajo);
+        echo "<br>";
     ?>
 
 </body>
